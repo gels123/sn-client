171780919cd2f41b52ea56e305f38fa36bd9b4c3
geliusheng <408963516@qq.com>
2023-08-30 16:23:21 +0800

    test luban

---
+++ Assets/Tests/TestLuban.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+using UnityEngine;
+
+public class TestLuban : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("== TestLuban Start ==" + Application.streamingAssetsPath);
+        var tables = new cfg.Tables(LoadByteBuf);
+        Debug.Log("加载"+tables.TbItem[1].ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private static JSONNode LoadByteBuf(string file)
+    {
+        return JSON.Parse(File.ReadAllText(Application.streamingAssetsPath +"/json/" + file + ".json", System.Text.Encoding.UTF8));
+    }
+}
