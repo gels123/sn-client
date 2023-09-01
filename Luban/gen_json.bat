323fb53ebc0cbd071106edb56917d0b1188f95ae
geliusheng <408963516@qq.com>
2023-09-01 17:48:59 +0800

    lit fix

--- Luban/gen_json.bat
+++ Luban/gen_json.bat
@@ -4,13 +4,13 @@ set GEN_CLIENT=%WORKSPACE%\Luban\Tools\Luban\Luban.dll
 set CONF_ROOT=%WORKSPACE%\Luban\Configs
 
 dotnet %GEN_CLIENT% ^
-    -t all ^
+    -t client ^
     -c cs-simple-json ^
     -d json  ^
     --schemaPath %CONF_ROOT%\Defines\__root__.xml ^
     -x inputDataDir=%CONF_ROOT%\Datas ^
     -x outputCodeDir=%WORKSPACE%\Assets\Scripts\Configs\Gen ^
     -x outputDataDir=%WORKSPACE%\Assets\StreamingAssets\Json ^
-    -x l10n.textProviderFile=*@%WORKSPACE%\Luban\Configs\Datas\l10n\texts.json 
+    -x l10n.textProviderFile=*@%WORKSPACE%\Luban\Configs\Datas\l10n\texts.json
         
 pause
