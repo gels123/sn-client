c3296f5717a3a7cdf4a7d78dd37571d541e4449d
geliusheng <408963516@qq.com>
2023-10-17 13:59:11 +0800

    small test

---
+++ Assets/Scripts/GameLaunch/test1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("xxxxxxxxxxxx1");
+        Logger.LogStackTrace("xxxxxxxxxxxx2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
